44505325e03ae664b6bdcbf9f0f32a098b99285a
csaba.solya@gmail.com <csaba.solya@gmail.com>
2016-06-27 13:31:02 +0200

    statisticsbuilder basic impl

---
+++ StatisticsBuilder/StatisticsBuilder/Models/ChangeList.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatisticsBuilder.Models
+{
+    public class ChangeList
+    {
+        [JsonProperty("Date")]
+        /// <summary>
+        /// Gets or sets the date when the changelist was submitted.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        [JsonProperty("Description")]
+        /// <summary>
+        /// Gets or sets the description of the changelist.
+        /// </summary>
+        public string Description { get; set; }
+
+        [JsonProperty("Id")]
+        /// <summary>
+        /// Gets or sets the id of the changelist.
+        /// </summary>
+        public int Id { get; set; }
+
+        [JsonProperty("Author")]
+        public string Author { get; set; }
+    }
+}
---
+++ StatisticsBuilder/StatisticsBuilder/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using StatisticsBuilder.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatisticsBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonFile = "changelists_Trainingset.json";
+            var jsonString = File.ReadAllText(jsonFile);
+
+            var allChangeLists = JsonConvert.DeserializeObject<List<ChangeList>>(jsonString);
+                        
+            foreach (var changelist in allChangeLists)
+            {
+
+            }
+        }
+    }
+}
