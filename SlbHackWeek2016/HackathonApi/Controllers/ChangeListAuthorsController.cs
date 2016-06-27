1979c8aadb4dc34496f00a1061b1ffd2b882ce55
illesj <jozsef_illes@epam.com>
2016-06-27 11:02:45 +0200

    add skeleton

---
+++ SlbHackWeek2016/HackathonApi/Controllers/ChangeListAuthorsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HackathonAPI.Models;
+
+namespace HackathonAPI.Controllers
+{
+    public class ChangeListAuthorsController : ApiController
+    {
+        /// <summary>
+        /// Try guessing who submitted the changelist to Perforce.
+        /// </summary>
+        /// <param name="changeList"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IHttpActionResult Guess([FromBody]ChangeList changeList)
+        {
+            MyBestGuess myGuess = new MyBestGuess();
+            myGuess.Id = changeList.Id;
+
+            //XXX Start working here:
+            //Use your algorithm to find out who was the author of the changelist.
+            //Set the MyBestGuess.Author property accordingly.
+
+            //Eg:
+            //if (changeList.Date > new DateTime(2014, 11, 12))
+            //{
+            //    changeList.Author = "TSanta";
+            //}
+
+            // You can start testing this api at http://localhost:10000/swagger
+            // glhf!
+
+            myGuess.Author = "John Doo";
+
+            return Ok(myGuess);
+        }
+    }
+
+    public class MyBestGuess
+    {
+        public string Author { get; set; }
+        public int Id { get; set; }
+    }
+
+}
---
+++ SlbHackWeek2016/HackathonApi/Models/ChangeList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HackathonAPI.Models
+{
+    /// <summary>
+    /// Represents a Perforce changelist.
+    /// </summary>
+    public class ChangeList
+    {
+        /// <summary>
+        /// Gets or sets the date when the changelist was submitted.
+        /// </summary>
+        public DateTime Date { get; set; }
+        /// <summary>
+        /// Gets or sets the description of the changelist.
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Gets or sets the id of the changelist.
+        /// </summary>
+        public int Id { get; set; }
+    }
+}
