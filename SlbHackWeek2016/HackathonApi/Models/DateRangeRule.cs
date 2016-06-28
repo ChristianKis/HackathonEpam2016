3dc69f7d506d90de40e5229c7252c2f416d0bf7d
illesj <jozsef_illes@epam.com>
2016-06-28 12:52:03 +0200

    refactor date range rule

--- SlbHackWeek2016/HackathonApi/Models/DateRangeRule.cs
+++ SlbHackWeek2016/HackathonApi/Models/DateRangeRule.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using HackathonAPI.Models;
 
 namespace GuessChangeListAuthor.Models
@@ -10,44 +7,52 @@ namespace GuessChangeListAuthor.Models
     {
         private class DTO
         {
-            public string Author { get; set; }
-            public DateTime minDate { get; set; }
-            public DateTime maxDate { get; set; }
+            public string Author;
+            public Dictionary<int, int[]> Commits;
+            public int AllCommits;
         }
 
-        private List<DTO> datas;
+        private readonly Dictionary<string, DTO> data = new Dictionary<string, DTO>();
 
         public void GenerateData(List<ChangeList> allChangeLists)
         {
-            datas = new List<DTO>();
-
-            var alma = from item in allChangeLists
-                       orderby item.Date
-                       group item by item.Author into grp
-                       select grp;
-
-            foreach (var item in alma)
+            foreach (var changeList in allChangeLists)
             {
-                var author = item.Key;
-                var min = item.Min(_ => _.Date);
-                var max = item.Max(_ => _.Date);
-
-                datas.Add(new DTO { Author = author, minDate = min, maxDate = max });
+                DTO dto;
+                var author = changeList.Author;
+                if (!data.TryGetValue(author, out dto))
+                {
+                    dto = new DTO { Author = author, Commits = new Dictionary<int, int[]>() };
+                    data.Add(author, dto);
+                }
+                var commits = dto.Commits;
+                int[] values;
+                var year = changeList.Date.Year;
+                if (!commits.TryGetValue(year, out values))
+                {
+                    values = new int[12];
+                    commits.Add(year, values);
+                }
+                var month = changeList.Date.Month;
+                commits[year][month]++;
+                dto.AllCommits++;
             }
         }
 
         public int Execute(string author, ChangeList cl)
         {
-            var dto = datas.FirstOrDefault(_ => _.Author == author);
-
-            if (dto == null)
+            DTO dto;
+            if (!data.TryGetValue(author, out dto))
                 return 0;
 
-            if (cl.Date >= dto.minDate && cl.Date <= dto.maxDate)
-                return 1;
-            
+            var year = cl.Date.Year;
+            int[] values;
+            if (!dto.Commits.TryGetValue(year, out values))
+                return 0;
 
-            return 0;
+            var month = cl.Date.Hour;
+            var chance = 100 * values[month] / dto.AllCommits;
+            return chance;
         }
     }
 }
